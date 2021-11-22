90b5b68e0848e6bd6cddfae52288b3da31628ee8
ariannejr <arianne.inoc@usjr.edu.ph>
2021-11-22 15:56:12 +0800

    Edited Routing

--- library-system/src/app/admin/admin.module.ts
+++ library-system/src/app/admin/admin.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
-
+import { AdminAddComponent } from './admin-add/admin-add.component';
+import { AdminCardComponent } from './admin-card/admin-card.component';
+import { AdminDeleteComponent } from './admin-delete/admin-delete.component';
+import { AdminEditComponent } from './admin-edit/admin-edit.component';
+import { AdminHomeComponent } from './admin-home/admin-home.component';
 
 @NgModule({
-  declarations: [
+  declarations: [ 
+    AdminAddComponent, 
+    AdminCardComponent, 
+    AdminDeleteComponent, 
+    AdminEditComponent, 
+    AdminHomeComponent, 
   ],
   imports: [
     CommonModule,
--- library-system/src/app/app.module.ts
+++ library-system/src/app/app.module.ts
@@ -17,11 +17,7 @@ import { AngularFireModule } from '@angular/fire/compat';
 
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { AdminAddComponent } from './admin/admin-add/admin-add.component';
-import { AdminCardComponent } from './admin/admin-card/admin-card.component';
-import { AdminDeleteComponent } from './admin/admin-delete/admin-delete.component';
-import { AdminEditComponent } from './admin/admin-edit/admin-edit.component';
-import { AdminHomeComponent } from './admin/admin-home/admin-home.component';
+
 import { LoginComponent } from './login/login.component';
 import { RegisterFormComponent } from './register/register-form/register-form.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
@@ -32,11 +28,7 @@ import { CompareValidatorDirective } from './helper/compare-validator.directive'
     AppComponent, 
     HomeComponent, 
     NotFoundComponent, 
-    AdminAddComponent, 
-    AdminCardComponent, 
-    AdminDeleteComponent, 
-    AdminEditComponent, 
-    AdminHomeComponent, 
+    
     LoginComponent, 
     RegisterFormComponent, 
     CompareValidatorDirective, 
