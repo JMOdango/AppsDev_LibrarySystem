1bbe3b79b2d9fa255f2d3526d2e17d1ad5aa21d1
Joan Marie Odango <joan.dango@usjr.edu.ph>
2021-11-22 19:59:18 +0800

    Changed route from log in to user

--- library-system/src/app/user/user-routing.module.ts
+++ library-system/src/app/user/user-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+import { UserHomeComponent } from './user-home/user-home.component';
 const routes: Routes = [
-   {
-
+  {
+    path: '',
+    component: UserHomeComponent ,
   },
 ];
 
