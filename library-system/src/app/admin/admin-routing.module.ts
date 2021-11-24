2a52c2140f4ca8c9531b43331a45db6393d01b29
ariannejr <arianne.inoc@usjr.edu.ph>
2021-11-24 23:36:13 +0800

    Made Changes

--- library-system/src/app/admin/admin-routing.module.ts
+++ library-system/src/app/admin/admin-routing.module.ts
@@ -2,10 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminHomeComponent } from './admin-home/admin-home.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-const routes: Routes = [ {
-  path: '',
-  component: AdminHomeComponent,
-},];
+
 
 const routes: Routes = [ {
   path: '',
