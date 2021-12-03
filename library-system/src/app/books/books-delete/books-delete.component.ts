de27e2a2623fec22af348d79a6c55351229504d3
ariannejr <arianne.inoc@usjr.edu.ph>
2021-12-03 23:58:07 +0800

    Edited Book Delete
    By putting the key or id, you can now delete a book off the database

--- library-system/src/app/books/books-delete/books-delete.component.ts
+++ library-system/src/app/books/books-delete/books-delete.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, OnInit, Output } from '@angular/core';
+import { BooksService } from 'src/app/shared/books.service';
 @Component({
   selector: 'app-books-delete',
   templateUrl: './books-delete.component.html',
   styleUrls: ['./books-delete.component.css']
 })
 export class BooksDeleteComponent implements OnInit {
-
-  constructor() { }
+  @Input() bookId: string;
+  @Output() deleted: boolean;
+  constructor(private crud: BooksService) { }
 
   ngOnInit(): void {
+  }
+
+  onDelete(id: string) {
+    this.crud.removeBooks(id);
+  }
+
+  onReuseDelete() {
+    this.crud.removeBooks(this.bookId);
   }
 
 }
